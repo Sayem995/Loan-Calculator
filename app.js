6747346e93bf6cddf693086bba0a9e8a5905f51d
Sayem995 <sayem.bokhari.1@gmail.com>
2020-06-07 20:47:25 +0500

    added UI variables to app.js

---
+++ app.js
@@ -0,0 +1,17 @@
+// listen for submit
+
+document.getElementById('loan-form').addEventListener('submit', calculateResults)
+;
+
+function calculateResults(e){
+    console.log('Calculating...');
+
+    const amount = document.getElementById('amount');
+    const interest = document.getElementById('interest');
+    const years = document.getElementById('years');
+    const monthlyPayment = document.getElementById('monthly-payment');
+    const totalPayment = document.getElementById('total-payment');
+    const totalInterest = document.getElementById('total-interest');
+}
+
+e.preventDefault();
